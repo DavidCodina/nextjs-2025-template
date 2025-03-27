f341f79d938d451bfb80aa499f67cd4fb279d2bc
David Codina <davidmcodina@icloud.com>
2025-03-27 16:49:31 -0600

    Updated to Next 15.2.4 (past auth vulnerability)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import { ComponentDemos } from '@/components/ComponentDemos'
 
 ======================================================================== */
 
+// Todo: add UI for the numeric position of slider thumbs.
+
+// Todo: Add not-found.tsx, global-error.tsx, error.tsx
+
 // Todo: Change all /components components to PascalCase & update components/index.ts
 //# Watch out for Vercel Gotcha - name append 'X' temporarily.
 
@@ -15,26 +19,16 @@ import { ComponentDemos } from '@/components/ComponentDemos'
 
 // Todo: Review whether render*BaseOnly is a good idea.
 
-// Todo: Update to latest Next.js due to recent auth security vulnerability.
-
 // Todo: Review https://react.dev/reference/react-dom/components/input
-
-// Todo: add UI for the numeric position of slider thumbs.
 
 // Todo: Buid out RHF controlled form demo.
 
-// Todo: Add not-found.tsx, global-error.tsx, error.tsx
-
 //# Not sure about hover:underline in AccordionTrigger.
 
-//# Find the radix primitive for allowing multiple panels to be open at once
+// Todo: Find the radix primitive for allowing multiple panels to be open at once
 //# on the accordion component.
 
 //# How can we change the offset in toolip and popover.
-
-//# Button has: aria-invalid:ring-destructive/20
-//# Something similar was on the badges, and I removed it.
-//# What is it?
 
 //# Test dropdown menu that stays open against sidebar and sheet.
 
